c421b4e15c7bcc9252219cc777366db129c61f10
Ansari-Zaki <ansarizaki7577@gmail.com>
2026-06-30 18:17:42 +0530

    Create TABLE ( CLIENTS ).sql
    New table create and clients details are added to it

---
+++ MY SQL/TABLE ( CLIENTS ).sql
@@ -0,0 +1,28 @@
+SHOW DATABASES;
+USE company;
+/*SHOW TABLES;
+CREATE TABLE clients(
+client_id INT PRIMARY KEY,
+client_name VARCHAR(50),
+project_name VARCHAR(50),
+toatl_cost DECIMAL(10,2),
+date_of_completion DATE,
+completion_status DECIMAL(10,2));
+SHOW TABLES;
+SHOW TABLES;
+INSERT INTO clients(client_id,client_name,project_name,toatl_cost,date_of_completion,completion_status)
+VALUES(111,"Space X","Website Development",35000,"2025-01-05",100),
+(112,"Riya","UI Development",50000,"2026-11-25",75),
+(113,"Apex Indusstries","HR Attrition Dashboard",60000,"2026-10-20",80),
+(114,"TechNova Pvt Ltd","Inventory",45000,"2025-09-05",85),
+(115,"Green Leaf Enterprises","Machine Maintance",80000,"2027-03-31",70),
+(116,"BlueSky","Web Design",50000,"2026-10-07",65),
+(117,"NextGen","Performance DashBoard",72000,"2026-03-31",82),
+(118,"NextGen","Asset Tracking System",100000,"2026-03-31",100),
+(119,"Apex Industries","Supply Chain Tracker",62000,"2025-08-12",90),
+(120,"BlueSky","Cash Maintance",40000,"2026-12-31",70),
+(121,"Security","Building Security",85000,"2025-12-31",80),
+(122,"Space X","App Development",50000,"2025-05-15",100);
+SELECT * FROM clients
+DESCRIBE clients
+SELECT * FROM clients*/
