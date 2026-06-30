fb9c93b0eb897026ad55e10fc3cca7e01ebbf985
Ansari-Zaki <ansarizaki7577@gmail.com>
2026-06-30 18:17:14 +0530

    Create READ ( RETRIVE ).sql
    Read ( RETRIVE ) data from the database

---
+++ MY SQL/READ ( RETRIVE ).sql
@@ -0,0 +1,15 @@
+/*USE company;
+SELECT * FROM employees
+ORDER BY emp_name
+SELECT emp_name,emp_dep,is_promoted FROM employees
+SELECT emp_name,emp_dep,is_promoted FROM employees
+ORDER BY emp_name
+SELECT * FROM employees
+WHERE department="Production" AND experience>2
+SELECT * FROM employees
+WHERE joining_date>"2020-01-01" AND joining_date<"2020-12-31"
+SELECT * FROM employees
+WHERE emp_dep="Service" AND branch="Mumbai" AND acadamic_percentage>50
+ORDER BY acadamic_percentage DESC
+SELECT * FROM employees
+WHERE emp_dep="Administration" AND is_promoted="TRUE" AND experience>2 AND acadamic_percentage>75*/
