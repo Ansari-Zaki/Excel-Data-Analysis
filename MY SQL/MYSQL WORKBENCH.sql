105bb3b962df662c6ffcca480b69044b84c98d0f
Ansari-Zaki <ansarizaki7577@gmail.com>
2026-06-30 18:15:54 +0530

    Create MYSQL WORKBENCH.sql
    First class of MY SQL

---
+++ MY SQL/MYSQL WORKBENCH.sql
@@ -0,0 +1,11 @@
+/*USE college;
+CREATE TABLE students(roll_no INT PRIMARY KEY auto_increment,
+student_name VARCHAR(50),
+gender VARCHAR(6),
+percentage DECIMAL(10,2))
+SHOW DATABASES
+INSERT INTO students(student_name, gender, percentage)
+VALUE("AHMED","MALE",65.20),
+("ABDULLAH","MALE",60.52),
+("FATIMA","FEMALE",75.82);
+SELECT * FROM students*/
