0985547ba0b900a73062e8332125fd7e49dd58f9
Ansari-Zaki <ansarizaki7577@gmail.com>
2026-06-30 18:16:30 +0530

    Create DATABASE ( COMPANY ).sql
    Create a company and insert records in it

---
+++ MY SQL/DATABASE ( COMPANY ).sql
@@ -0,0 +1,28 @@
+/*CREATE DATABASE company;
+SHOW DATABASES
+USE company;
+CREATE TABLE employees(
+emp_id INT PRIMARY KEY AUTO_INCREMENT,
+emp_name VARCHAR(50),
+emp_dep VARCHAR(50),
+joining_date DATE,
+is_promoted BOOLEAN,
+branch VARCHAR(50),
+education VARCHAR(50),
+department VARCHAR(50),
+job_title VARCHAR(50),
+experience INT,
+acadamic_percentage DECIMAL(10,2));
+SHOW TABLES
+INSERT INTO employees (emp_name,emp_dep,joining_date,is_promoted,branch,education,department,job_title,experience,acadamic_percentage)
+VALUES("Areeb","Sales","2020-02-25",TRUE,"Ahmadabad","DTL","Production","Manager",3,52.60),
+("Affan","Service","2022-05-05",TRUE,"Mumbai","ITA","Packaging","Worker",4,45.85),
+("Kaleem","Administration","2023-07-15",FALSE,"Hydrabad","BCom","Service","Assistant Manager",2,86.20),
+("Abdullah","Sales","2025-12-01",FALSE,"Kolkata","MBA","Sales","Supervisor",5,88),
+("Hassan","Service","2018-04-05",TRUE,"Mumbai","BA","Production","Director",7,72.15),
+("Faisal","Administration","2020-08-20",FALSE,"Nashik","HSC","Serive","Manager",6,76.40),
+("Abdurrhaman","Service","2022-11-21",TRUE,"Mumbai","BA","Packaging","Union Leader",3,70.71),
+("Mohammed","Administration","2021-01-30",TRUE,"Ahmadabad","MCom","Sales","Administrator",1,83.83),
+("Hashir","Sales","2024-4-27",FALSE,"Kolkata","BCom","Production","Worker",0,71.60),
+("Aadil","Service","2025-6-17",TRUE,"Hydrabad","BCom","Service","Manager",2,60.40);
+SELECT * FROM employees*/
