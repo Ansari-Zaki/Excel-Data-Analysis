8f757f24f3c32c0a28ab1452f92ba25c30d816f7
Ansari-Zaki <ansarizaki7577@gmail.com>
2026-07-01 18:08:57 +0530

    Create Update & Delete.sql
    Delete & Update Qurey LearN

---
+++ MY SQL/Update & Delete.sql
@@ -0,0 +1,28 @@
+USE company;
+/*SELECT * FROM clients;
+UPDATE clients
+SET client_name="Security",
+project_name="Cyber Security"
+WHERE client_id=121;
+SELECT * FROM clients;
+UPDATE clients
+SET client_name="Cyber Hub",
+project_name="Cyber Security"
+WHERE client_id=121
+SELECT * FROM clients;
+DELETE FROM clients
+WHERE client_name="Space X"
+SELECT * FROM clients
+DELETE FROM clients 
+WHERE completion_status<70
+SELECT * FROM clients
+INSERT INTO clients(client_id,client_name,project_name,toatl_cost,date_of_completion,completion_status)
+VALUES (122,"Crown Hotel","Project Launch Meeting",70000,"2026-07-05",90),
+(123,"TATA","Online Service",76000,"2026-12-31",83),
+(124,"Built","New Building",150000,"2028-03-31",35),
+(125,"Bajaj Finance","Loan Acquition",95000,"2026-05-01","78"),
+(126,"Claude","AI Buidling",80000,"2026-05-01","80"),
+(127,"Pixel Forge","Domain Buiding",99000,"2026-05-01","85"),
+(128,"Crown Hotel","Project Launch Meeting",70000,"2026-05-01","90"),
+(129,"Crown Hotel","Project Launch Meeting",70000,"2026-05-01","90");*/
+SELECT * FROM clients
