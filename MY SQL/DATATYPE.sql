913ed4a135b1437a20a09c78b3be6adb9e7e5682
Ansari-Zaki <ansarizaki7577@gmail.com>
2026-07-04 18:07:14 +0530

    Create DATATYPE.sql
    MY SQL

---
+++ MY SQL/DATATYPE.sql
@@ -0,0 +1,19 @@
+/*SHOW DATABASES;
+CREATE DATABASE practice;
+SHOW DATABASES;
+SHOW DATABASES*/
+USE practice;
+/*SHOW TABLES;
+CREATE TABLE profile(
+profile_id INT PRIMARY KEY AUTO_INCREMENT,
+ssc_seat_no CHAR(8),
+hsc_seat_no CHAR(8),
+user_name VARCHAR(50),
+gender ENUM("Male","Female"),
+skill SET("Python","C++","Java","Excel","SQL","Power BI"),
+career_objective TEXT(1000),
+date_of_birth DATE,
+batch_time TIME,
+attendance_time TIMESTAMP);
+SHOW TABLES;*/
+
